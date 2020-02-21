a2eb4eac3c4ebaa1061344b33979f5c1812f5529
chloele33 <chloele@seas.upenn.edu>
2020-02-20 17:38:34 -0800

    home page fade in

--- js/home.js
+++ js/home.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
   if (clickedFromSplash === "1") {
     setTimeout(function() {
       $(".page").removeClass("animate_content");
+      $(".flex-container").removeClass("fade");
       $("body").css("background-color", "white");
     }, 1500);
     window.sessionStorage.setItem("splashVisited", 0);
   } else {
     $(".page").removeClass("animate_content");
+    $(".flex-container").removeClass("fade");
     $("body").css("background-color", "white");
   }
 });
