b6d0f64bddfed82c8d118fa1f0807031e37fb6d9
chloele33 <chloele@seas.upenn.edu>
2020-03-22 21:02:30 -0400

    fix scroll

--- js/home.js
+++ js/home.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   var clickedFromSplash = window.sessionStorage.getItem("splashVisited");
 
   if (clickedFromSplash === "1") {
+    window.sessionStorage.setItem("splashVisited", 0);
+
     setTimeout(function() {
-      $(".page").removeClass("animate_content");
       $(".flex-container").removeClass("fade");
       $("body").css("background-color", "white");
     }, 3000);
-    window.sessionStorage.setItem("splashVisited", 0);
   } else {
     $(".page").removeClass("animate_content");
     $(".flex-container").removeClass("fade");
